c69a55afdb8a32832c92a3d84f37878c796dc2db
cilikchen <48408321+cilikchen@users.noreply.github.com>
2020-11-18 21:30:41 +0000

    Create LeetCode57.java

---
+++ LeetCode57.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+public class LeetCode57 {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        Arrays.sort(intervals,(a,b)->Integer.compare(a[0] ,b[0]));
+        List<int[]> result = new ArrayList<>();
+        for(int[] i : intervals){
+            if(newInterval == null || i[1] < newInterval[0]){
+                result.add(i);
+            }else if(i[0] > newInterval[1]){
+                result.add(newInterval);
+                result.add(i);
+                newInterval = null;
+            }else{
+                newInterval[0] = Math.min(newInterval[0], i[0]);
+                newInterval[1] = Math.max(newInterval[1], i[1]);
+            }
+        }
+        if(newInterval != null)
+            result.add(newInterval);
+        return result.toArray(new int[result.size()][]);
+    }
+}
