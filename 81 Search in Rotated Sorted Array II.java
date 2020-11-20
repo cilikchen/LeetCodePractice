04ff6443db9d0e9c1ff33c15a46e97acf802e91c
cilikchen <48408321+cilikchen@users.noreply.github.com>
2020-11-20 09:55:12 +0000

    Update 81 Search in Rotated Sorted Array II.java

--- 81 Search in Rotated Sorted Array II.java
+++ 81 Search in Rotated Sorted Array II.java
@@ -1,10 +1,42 @@
 public class LeetCode81 {
     public boolean search(int[] nums, int target) {
+        int head = nums.length;
+        int tail =0;
+
+        while (tail < head){
+            int mid =(head + tail)/2 ;
+            if(nums[mid] == target){
+                return true;
+            }
+            if(nums[mid] > nums[tail]){
+                if(target >=nums[tail] && target <nums[mid]){
+                    head = mid;
+                }else{
+                    tail = mid+1;
+                }
+            }else if(nums[mid] < nums[tail]){
+                if (target > nums[mid] && target < nums[tail]){
+                    tail = mid+1;
+                }else{
+                    head = mid;
+                }
+            }else{
+                tail++;
+            }
+
+        }
+        return  false;
+
+
+
+/*
         for(int i=0;i<nums.length;i++){
             if(nums[i] == target){
                 return true;
             }
         }
         return false;
+
+        */
     }
 }
