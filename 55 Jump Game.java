98d7cf9516b6e608ceadfbe6c70ff7848fe7237b
cilikchen <48408321+cilikchen@users.noreply.github.com>
2020-12-07 17:30:29 +0000

    Create 55 Jump Game.java

---
+++ 55 Jump Game.java
@@ -0,0 +1,21 @@
+public class LeetCode55 {
+    public boolean canJump(int[] nums) {
+        boolean[] dp = new boolean[nums.length];
+        dp[0] = true;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (dp[i]) {
+                for (int j = 1; j <= nums[i]; j++) {
+                    if (i + j < nums.length && !dp[i + j]) {
+                        dp[i + j] = true;
+                        if (i + j == nums.length) {
+                            return true;
+                        }
+                    }
+
+                }
+            }
+        }
+        return dp[nums.length - 1];
+    }
+}
