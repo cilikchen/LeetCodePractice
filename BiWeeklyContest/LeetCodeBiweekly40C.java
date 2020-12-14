fa93f01794a02e392a2b2bc5c9e0f23381a99d82
cilikchen <48408321+cilikchen@users.noreply.github.com>
2020-12-14 07:30:04 +0000

    Create LeetCodeBiweekly40C.java

---
+++ BiWeeklyContest/LeetCodeBiweekly40C.java
@@ -0,0 +1,65 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+
+public class LeetCodeBiweekly40C {
+
+    ArrayList<Integer> list = new ArrayList<>();
+
+    public void FrontMiddleBackQueue() {
+
+    }
+
+    public void pushFront(int val) {
+        list.add(0,val);
+
+    }
+
+    public void pushMiddle(int val) {
+        int n = list.size();
+
+            list.add(n/2,val);
+
+
+    }
+
+    public void pushBack(int val) {
+        list.add(val);
+
+    }
+
+    public int popFront() {
+        if(list.isEmpty()){
+            return -1;
+        }
+        int ans= list.get(0);
+        list.remove(0);
+        return ans;
+
+
+    }
+
+    public int popMiddle() {
+        if(list.isEmpty()){
+            return -1;
+        }
+        int n = list.size();
+        int ans =0;
+
+        ans = list.get((n-1)/2);
+        list.remove((n-1)/2);
+
+
+        return ans;
+    }
+
+    public int popBack() {
+        if(list.isEmpty()){
+            return -1;
+        }
+        int ans = list.get(list.size()-1);
+        list.remove(list.size()-1);
+        return ans;
+
+    }
+}
