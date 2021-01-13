403ff834dc1eec7bfb159b005e8623d1d4efd4e6
cilikchen <48408321+cilikchen@users.noreply.github.com>
2021-01-13 10:40:46 +0000

    Create 881 Boats to Save People.java
    using two pointer code 
    for loop from sample solution

---
+++ 881 Boats to Save People.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class LeetCode881 {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int i, j;
+        for (i = 0, j = people.length - 1; i <= j; --j){
+            if (people[i] + people[j] <= limit) ++i;
+        }
+            return people.length - 1 - j;
+
+    }
+}
