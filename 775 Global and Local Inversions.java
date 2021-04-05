dc90eaab3f8b07cbebd6d780846015df1b96eabf
cilikchen <48408321+cilikchen@users.noreply.github.com>
2021-04-05 10:19:45 +0100

    Create 775 Global and Local Inversions.java
    from lee215 not mine idea

---
+++ 775 Global and Local Inversions.java
@@ -0,0 +1,11 @@
+public class LeetCode775 {
+    public boolean isIdealPermutation(int[] A) {
+        int cmax = 0;
+        for (int i = 0; i < A.length - 2; ++i) {
+            cmax = Math.max(cmax, A[i]);
+            if (cmax > A[i + 2]) return false;
+        }
+        return true;
+    }
+
+}
